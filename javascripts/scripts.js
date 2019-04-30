d4d3760d85b5139641fd7b36624da400e30041e7
Cesare Ferrari <cesare@maryandferrari.com>
2019-04-29 21:55:56 -0400

    Style menu for mobile and desktop

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,27 +1,11 @@
 const menuButtons = document.querySelectorAll('.menu-btn');
-const mobileNavigation = document.querySelector('nav.full');
+const mobileNavigation = document.querySelector('header nav');
 const signinButton = document.querySelector('#signin-button');
-const mobileStyles = "display: flex; flex-direction: column; margin-top: 0; height: initial; border-top: 1px solid white";
 
 menuButtons.forEach(button => button.addEventListener('click', function(event) {
   console.log('Clicked: ', event.target);
-
-
-  if (mobileNavigation.style.display === 'block') {
-    mobileNavigation.style.display = 'none';
-  } else {
-    signinButton.classList.remove('button', 'white-button');
-
-    mobileNavigation.style.cssText = mobileStyles;
-
-
-
-//    mobileNavigation.style.display = 'flex';
-//    mobileNavigation.classList.add('mobile-open');
-//    mobileNavigation.style.flexDirection = 'column';
-  }
+  signinButton.classList.remove('button', 'white-button');
+  mobileNavigation.classList.toggle('hide');
 }))
 
 
-
-// elt.style.cssText = "color: blue; border: 1px solid black"; 
