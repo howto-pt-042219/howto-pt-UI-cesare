62bc52684806e1749238b4c6b4c75521d2e5208b
Cesare Ferrari <cesare@maryandferrari.com>
2019-05-01 23:00:53 -0400

    Add team description

--- components/carousel/carousel.js
+++ components/carousel/carousel.js
@@ -1,8 +1,34 @@
-const carousel = document.querySelector('.carousel');
+class Carousel {
+  constructor(carouselElement) {
+    this.carouselElement = carouselElement;
+    //this.slides = document.querySelectorAll('.slide');
+    this.slides = [];
+    document.querySelectorAll('.slide').forEach(slide => {
+      this.slides.push(new Slide(slide));
+    });
+  }
+}
 
+class Slide {
+  constructor(slideElement) {
+    this.slideElement = slideElement;
+    this.slideElement.classList.add('slide-hide');
+    this.name = this.slideElement.dataset.name;
+  }
+}
 
-const openCarousel = function() {
-  carousel.classList.remove('hide');
+const carousel = new Carousel(document.querySelector('.carousel'));
+
+const openCarousel = function(event) {
+  event.preventDefault();
+  name = event.target.dataset.name;
+  slide = carousel.slides.find(function(slide) {
+    return slide.name === name;
+  })
+
+  carousel.carouselElement.classList.remove('hide');
+  carousel.slides.forEach(slide => slide.slideElement.classList.add('slide-hide'));
+  slide.slideElement.classList.remove('slide-hide');
 }
 
 const moreButtons = document.querySelectorAll('.more');
